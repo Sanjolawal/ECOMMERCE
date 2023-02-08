30e205489d079075a414aaa2a90d9bf606bb8fbe
sanjo lawal <sanjolawal600@gmail.com>
2023-02-08 05:18:51 -0800

    fixes

--- app.js
+++ app.js
@@ -10,18 +10,17 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Server is listening`));
 
-app.use(express.static("dist"));
-
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "dist", "index.html"))
-);
-
 const Server = async () => {
   try {
     const connection = await mongoose.connect(process.env.uri);
     console.log(`Connected to Database`);
     app.use(router);
+    app.use(express.static("dist"));
+
+    const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "dist", "index.html"))
+    );
   } catch (error) {
     console.log(error.message);
   }
